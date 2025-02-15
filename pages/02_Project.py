8e359b8426d8a084f00781cbb6a9d87b9c2320b9
Mushtaq Ali <ali10786@gmail.com>
2025-02-15 22:02:48 +0100

    Update 02_Project.py

--- pages/02_Project.py
+++ pages/02_Project.py
@@ -6,7 +6,7 @@ from footer import render_footer
     #st.write("This is the page for project details")
 image = Image.open('wf.png')
 	    
-col1, mid, col2 = st.columns([40,0.5,0.5])
+col1, mid, col2 = st.columns([30,0.5,0.5])
 with col1:
         st.image(image,use_column_width=False)
 with col2:
